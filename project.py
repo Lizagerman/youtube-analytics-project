d1d50c53136e42de63bb850ce3a0163102ac9d17
Liza G <110494499+Lizagerman@users.noreply.github.com>
2023-07-02 18:54:54 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,18 +1,1 @@
-[tool.poetry]
-name = "youtube-analytics-project"
-version = "0.1.0"
-description = ""
-authors = ["Alexandr Abramov <alexandr.abramovvv@yandex.ru>"]
-readme = "README.md"
-packages = [{include = "youtube_analytics_project"}]
 
-[tool.poetry.dependencies]
-python = "^3.11"
-
-
-[tool.poetry.group.dev.dependencies]
-google-api-python-client = "^2.80.0"
-
-[build-system]
-requires = ["poetry-core"]
-build-backend = "poetry.core.masonry.api"
